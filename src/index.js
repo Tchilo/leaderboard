45dc0943c7dcc5190d0cc1065b773b845d6ad4a2
Tchilo <tchiloross@gmail.com>
2021-10-05 22:03:56 +0200

    slect elements from html and add event listeners

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import './style.css';
+
+const grab = (e, isId = false, qAll = false) => {
+  if (isId) {
+    return document.getElementById(e);
+  } if (qAll) {
+    return document.querySelectorAll(`.${e}`);
+  }
+  return document.querySelector(`.${e}`);
+};
+
+const refBtn = grab('ref-btn', true);
+const sub = grab('sub', true);
+const name = grab('name', true);
+const score = grab('score', true);
+
+sub.addEventListener('click', () => {
+  alert(name.value, score.value);
+});
+
+
+
+
+refBtn.addEventListener('click', () => {
+
+});
