cbae38e3c2909b2a9e18050574a7083fb5015c7e
Tchilo <tchiloross@gmail.com>
2021-10-06 21:02:03 +0200

    Get and display data function

--- src/index.js
+++ src/index.js
@@ -2,3 +2,18 @@ const grab = (e) => document.getElementById(e);
 
 const refBtn = grab('ref-btn');
 const ol = grab('list');
+
+const refresh = async (gameID) => {
+  fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameID}/scores/`)
+    .then((response) => response.json())
+    .then((data) => {
+      ol.innerHTML = '';
+      const scores = data.result;
+      for (let index = 0; index < scores.length; index++) {
+        const element = scores[index];
+        const template = document.createElement('template');
+        template.innerHTML = `<li>${element.user}:${element.score}</li>`;
+        ol.appendChild(template.content.firstChild);
+      }
+    });
+};
